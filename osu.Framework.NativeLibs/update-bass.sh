81ce3cf49b84062cdcdb7be55a05d7076856fd02
LA <1245661240@qq.com>
2025-12-29 20:25:40 +0800

    升级包

--- osu.Framework.NativeLibs/update-bass.sh
+++ osu.Framework.NativeLibs/update-bass.sh
@@ -74,5 +74,10 @@ unzip -qjo bassmix24-android.zip libs/arm64-v8a/* -d ../osu.Framework.Android/ar
 unzip -qjo bassmix24-android.zip libs/armeabi-v7a/* -d ../osu.Framework.Android/armeabi-v7a/
 unzip -qjo bassmix24-android.zip libs/x86/* -d ../osu.Framework.Android/x86/
 
+# bassasio
+curl -Lso bassasio.zip https://www.un4seen.com/stuff/bassasio.zip
+unzip -qjo bassasio.zip x64/bassasio.dll -d runtimes/win-x64/native/
+unzip -qjo bassasio.zip bassasio.dll -d runtimes/win-x86/native/
+
 # clean up
 rm bass*.zip
